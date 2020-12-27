990f9759980acbd7f12f01718f84b98b6ca29898
Celso Ribeiro <celsofilho82@gmail.com>
2020-12-27 12:20:50 -0300

    criando o projeto

---
+++ api-bank/src/main/java/com/orange/talents/apibank/ApiBankApplication.java
@@ -0,0 +1,13 @@
+package com.orange.talents.apibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBankApplication.class, args);
+	}
+
+}
