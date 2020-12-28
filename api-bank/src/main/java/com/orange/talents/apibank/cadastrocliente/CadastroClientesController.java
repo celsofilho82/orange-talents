fb0fb2f70d6bcb5a63968100d228d80b7cbed88c
Celso Ribeiro <celsofilho82@gmail.com>
2020-12-28 11:43:31 -0300

    inserindo validador de email e cpf no controller

--- api-bank/src/main/java/com/orange/talents/apibank/cadastrocliente/CadastroClientesController.java
+++ api-bank/src/main/java/com/orange/talents/apibank/cadastrocliente/CadastroClientesController.java
@@ -6,10 +6,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.orange.talents.apibank.validator.CpfUnicoValidator;
+import com.orange.talents.apibank.validator.EmailUnicoValidator;
 
 @RestController
 @RequestMapping("/clientes")
@@ -17,7 +22,18 @@ public class CadastroClientesController {
 
 	@Autowired
 	private EntityManager manager;
-	
+
+	@Autowired
+	private EmailUnicoValidator emailUnicoValidator;
+
+	@Autowired
+	private CpfUnicoValidator cpfUnicoValidator;
+
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		binder.addValidators(emailUnicoValidator, cpfUnicoValidator);
+	}
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity<Cliente> create(@RequestBody @Valid NovoClienteRequest request) {
